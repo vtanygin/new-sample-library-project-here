5572dbf4ff1a76d9e6600a65a591a6421c2301fe
vtanygin <63617394+vtanygin@users.noreply.github.com>
2026-04-04 20:47:29 -0400

    initial

---
+++ from-scratch-WPF-sample-library/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace from_scratch_WPF_sample_library
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
